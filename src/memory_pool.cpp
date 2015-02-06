414ee535f786051f0ccf0959e11c491462820142
Cameron Hart <cam@bitshifter.net.nz>
2015-02-06 22:35:35 +1100

    Moved tests into memory_pool.cpp and other tidying.

--- src/memory_pool.cpp
+++ src/memory_pool.cpp
@@ -1,4 +1,4 @@
-#include "memory_pool.h"
+#include "memory_pool.hpp"
 
 #include <cassert>
 #include <cstdlib>
@@ -35,6 +35,12 @@ namespace {
 		// create a word with a single 1-bit at the position of the rightmost
 		// 0-bit in x, producing 0 if none, then count the trailing zeros.
 		return __builtin_ctz(~n & (n + 1));
+	}
+
+	/// Returns true if the pointer is of the given alignment
+	inline bool is_aligned(const void * ptr, size_t align)
+	{
+		return (reinterpret_cast<uintptr_t>(ptr) & (align - 1)) == 0;
 	}
 }
 
@@ -190,3 +196,73 @@ bool MemoryPoolBase::check_generation(uint32_t generation) const
 	return generation_ == generation;
 }
 
+//
+// Tests
+//
+
+#include "catch.hpp"
+
+TEST_CASE("Single new and delete", "[allocation]")
+{
+	MemoryPool<uint32_t> mp;
+	uint32_t * p = mp.new_object(0xaabbccdd);
+	REQUIRE(p != nullptr);
+	CHECK(is_aligned(p, 4));
+	CHECK(*p == 0xaabbccdd);
+	mp.delete_object(p);
+}
+
+TEST_CASE("Double new and delete", "[allocation]")
+{
+	MemoryPool<uint32_t> mp;
+	uint32_t * p1 = mp.new_object(0x11223344);
+	REQUIRE(p1 != nullptr);
+	CHECK(is_aligned(p1, 4));
+	uint32_t * p2 = mp.new_object(0x55667788);
+	REQUIRE(p2 != nullptr);
+	CHECK(is_aligned(p2, 4));
+	CHECK(*p1 == 0x11223344);
+	mp.delete_object(p1);
+	CHECK(*p2 == 0x55667788);
+	mp.delete_object(p2);
+}
+
+TEST_CASE("Block fill and free", "[allocation]")
+{
+	MemoryPool<uint32_t> mp;
+	std::vector<uint32_t *> v;
+	for (size_t i = 0; i < 64; ++i)
+	{
+		uint32_t * p = mp.new_object(1 << i);
+		REQUIRE(p != nullptr);
+		CHECK(*p == 1 << i);
+		v.push_back(p);
+	}
+	for (auto p : v)
+	{
+		mp.delete_object(p);
+	}
+}
+
+TEST_CASE("Iterate full blocks", "[iteration]")
+{
+	MemoryPool<uint32_t> mp;
+	std::vector<uint32_t *> v;
+	for (size_t i = 0; i < 64; ++i)
+	{
+		uint32_t * p = mp.new_object(1 << i);
+		REQUIRE(p != nullptr);
+		CHECK(*p == 1 << i);
+		v.push_back(p);
+	}
+	size_t index = 0;
+	for (auto itr = mp.begin(), end = mp.end(); itr != end; ++itr)
+	{
+		CHECK(*itr == 1 << index);
+		++index;
+	}
+	for (auto p : v)
+	{
+		mp.delete_object(p);
+	}
+}
