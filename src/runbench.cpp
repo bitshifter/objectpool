3b94cbf92048788d8f56fd50506493e2b28fe638
Cameron Hart <cameron.hart@gmail.com>
2016-07-06 21:52:26 +1000

    Add benchmark comparisons with boost pool is available

--- src/runbench.cpp
+++ src/runbench.cpp
@@ -2,6 +2,10 @@
 #include "bench.hpp"
 #include "object_pool.hpp"
 
+#ifdef BENCH_BOOST_POOL
+#include <boost/pool/object_pool.hpp>
+#endif
+
 /// Test which allocates a number of objects then frees them all
 template <typename T>
 void alloc_free(T& pool)
@@ -56,19 +60,15 @@ public:
     {
     }
     void new_index(size_t i) { ptr[i] = pool.new_object(); }
-    void delete_index(size_t i)
-    {
-        pool.delete_object(ptr[i]);
-        ptr[i] = nullptr;
-    }
     void delete_all()
     {
         // delete individual objects for fair comparison
-        for (size_t i = 0; i < ptr.size(); i++)
-        {
-            pool.delete_object(ptr[i]);
-            ptr[i] = nullptr;
-        }
+        pool.delete_all();
+        //for (auto & p : ptr)
+        //{
+        //    pool.delete_object(p);
+        //    p = nullptr;
+        //}
     }
     template <typename F>
     void for_each(const F func) const
@@ -101,25 +101,20 @@ public:
 
     SizedHeapAlloc(size_t /*block_size*/, size_t allocs) : ptr(allocs, nullptr) {}
     void new_index(size_t i) { ptr[i] = new value_t; }
-    void delete_index(size_t i)
-    {
-        delete ptr[i];
-        ptr[i] = nullptr;
-    }
     void delete_all()
     {
-        for (size_t i = 0; i < ptr.size(); i++)
+        for (auto & p : ptr)
         {
-            delete ptr[i];
-            ptr[i] = nullptr;
+            delete p;
+         //   p = nullptr;
         }
     }
     template <typename F>
     void for_each(const F func) const
     {
-        for (size_t i = 0; i < ptr.size(); i++)
+        for (auto p : ptr)
         {
-            func(ptr[i]);
+            func(p);
         }
     }
     size_t count() const { return ptr.size(); }
@@ -136,6 +131,55 @@ private:
     std::vector<value_t*> ptr;
 };
 #endif // BENCH_HEAP_ALLOC
+
+#ifdef BENCH_BOOST_POOL
+template <size_t N>
+class SizedBoostAlloc
+{
+public:
+    typedef Sized<N> value_t;
+    typedef boost::object_pool<value_t> PoolT;
+
+    SizedBoostAlloc(size_t block_size, size_t allocs)
+        : pool(new PoolT(block_size, allocs)), ptr(allocs, nullptr), block_size(block_size), allocs(allocs)
+    {
+    }
+    void new_index(size_t i) { ptr[i] = pool->construct(); }
+    void delete_all()
+    {
+        // boost pool cleans up all objects on destruction
+        pool.reset(new PoolT(block_size, allocs));
+        //for (auto & p : ptr)
+        //{
+        //    pool.destroy(p);
+        //    p = nullptr;
+        //}
+    }
+    template <typename F>
+    void for_each(const F func) const
+    {
+        for (auto p : ptr)
+        {
+            func(p);
+        }
+    }
+    size_t count() const { return ptr.size(); }
+    void memset(int value)
+    {
+        const size_t value_size = sizeof(value_t);
+        for (auto p : ptr)
+        {
+            ::memset(p, value, value_size);
+        }
+    }
+
+private:
+    std::unique_ptr<PoolT> pool;
+    std::vector<value_t*> ptr;
+    size_t block_size;
+    size_t allocs;
+};
+#endif // BENCH_BOOST_POOL
 
 // I am a bad person. Bad and lazy.
 
@@ -167,39 +211,51 @@ private:
         allocs)
 #define HEAP_BENCH_TEST(run, value_size, block_size)                                               \
     _CONFIG_BENCH_TEST(SizedHeapAlloc<value_size>, heap, run, value_size, block_size, block_size)
+#if BENCH_BOOST_POOL
+#define BOOST_BENCH_TEST(run, value_size, block_size)                                              \
+    _CONFIG_BENCH_TEST(SizedBoostAlloc<value_size>, boost_pool, run, value_size, block_size, block_size)
+#else
+#define BOOST_BENCH_TEST(run, value_size, block_size)
+#endif
 
 FIXED_POOL_BENCH_TEST(alloc_free, 16, 1000)
 DYNAMIC_POOL_BENCH_TEST(alloc_free, 16, 64, 1000)
 DYNAMIC_POOL_BENCH_TEST(alloc_free, 16, 128, 1000)
 DYNAMIC_POOL_BENCH_TEST(alloc_free, 16, 256, 1000)
 HEAP_BENCH_TEST(alloc_free, 16, 1000)
+BOOST_BENCH_TEST(alloc_free, 16, 1000)
 
 FIXED_POOL_BENCH_TEST(alloc_free, 128, 1000)
 DYNAMIC_POOL_BENCH_TEST(alloc_free, 128, 64, 1000)
 DYNAMIC_POOL_BENCH_TEST(alloc_free, 128, 128, 1000)
 DYNAMIC_POOL_BENCH_TEST(alloc_free, 128, 256, 1000)
 HEAP_BENCH_TEST(alloc_free, 128, 1000)
+BOOST_BENCH_TEST(alloc_free, 128, 1000)
 
 FIXED_POOL_BENCH_TEST(alloc_free, 512, 1000)
 DYNAMIC_POOL_BENCH_TEST(alloc_free, 512, 64, 1000)
 DYNAMIC_POOL_BENCH_TEST(alloc_free, 512, 128, 1000)
 DYNAMIC_POOL_BENCH_TEST(alloc_free, 512, 256, 1000)
 HEAP_BENCH_TEST(alloc_free, 512, 1000)
+BOOST_BENCH_TEST(alloc_free, 512, 1000)
 
 FIXED_POOL_BENCH_TEST(alloc_memset_free, 16, 1000)
 DYNAMIC_POOL_BENCH_TEST(alloc_memset_free, 16, 64, 1000)
 DYNAMIC_POOL_BENCH_TEST(alloc_memset_free, 16, 128, 1000)
 DYNAMIC_POOL_BENCH_TEST(alloc_memset_free, 16, 256, 1000)
 HEAP_BENCH_TEST(alloc_memset_free, 16, 1000)
+BOOST_BENCH_TEST(alloc_memset_free, 16, 1000)
 
 FIXED_POOL_BENCH_TEST(alloc_memset_free, 128, 1000)
 DYNAMIC_POOL_BENCH_TEST(alloc_memset_free, 128, 64, 1000)
 DYNAMIC_POOL_BENCH_TEST(alloc_memset_free, 128, 128, 1000)
 DYNAMIC_POOL_BENCH_TEST(alloc_memset_free, 128, 256, 1000)
 HEAP_BENCH_TEST(alloc_memset_free, 128, 1000)
+BOOST_BENCH_TEST(alloc_memset_free, 128, 1000)
 
 FIXED_POOL_BENCH_TEST(alloc_memset_free, 512, 1000)
 DYNAMIC_POOL_BENCH_TEST(alloc_memset_free, 512, 64, 1000)
 DYNAMIC_POOL_BENCH_TEST(alloc_memset_free, 512, 128, 1000)
 DYNAMIC_POOL_BENCH_TEST(alloc_memset_free, 512, 256, 1000)
 HEAP_BENCH_TEST(alloc_memset_free, 512, 1000)
+BOOST_BENCH_TEST(alloc_memset_free, 512, 1000)
